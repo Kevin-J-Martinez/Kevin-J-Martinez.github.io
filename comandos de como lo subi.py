e47fe152f9f0f135ba046fc46829db375876821d
Kevin Martinez <inversiones.personales.inc@gmail.com>
2026-09-14 14:39:37 -0500

    Update website

---
+++ comandos de como lo subi.py
@@ -0,0 +1,8 @@
+# git remote add origin https://github.com/Kevin-J-Martinez/Kevin-J-Martinez.github.io.git
+# git branch -M main 
+# git add --all
+# git commit -m "inicial commit"
+# git push -u origin main
+#tree /F /A > estructura.txt comando para crear un doc de la estrucutra
+# quarto render compilar y guardar cambios -> git add --all -> git commit -m "Update website" -> git push
+# quarto preview para previsualizzar 
